f2334f112ba538259460b2c593a375c672c2ec71
MOHAMMED THALHA J <96979827+Mohammed-Thalha@users.noreply.github.com>
2022-09-22 21:39:18 +0530

    Create 13. Int to Double.java

---
+++ Basic_Java_Programs/13. Int to Double.java
@@ -0,0 +1,12 @@
+// Widening Type Casting (Lower to higher)
+import java.util.Scanner;
+class Main{
+public static void main(String[] args){
+Scanner sc = new Scanner(System.in);
+System.out.println("Enter the number : ");
+int num1 = sc.nextInt();
+double a=num1;
+System.out.println("Value of integer is : "+num1);
+System.out.println("Converted from int to double : "+a);
+}
+}
