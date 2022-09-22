6b223df26eebc8362ae6eb3b9c6fe1d89ce01a96
MOHAMMED THALHA J <96979827+Mohammed-Thalha@users.noreply.github.com>
2022-09-22 21:22:12 +0530

    Create 02. Hello World.java

---
+++ Basic_Java_Programs/02. Hello World.java
@@ -0,0 +1,11 @@
+// Hello World using Scanner class
+
+import java.util.Scanner;
+class Main{
+public static void main(String[] args){
+Scanner sc = new Scanner(System.in);
+System.out.println("Enter the String : ");
+String str = sc.nextLine();
+System.out.println("Entered String is : "+str);
+}
+}
