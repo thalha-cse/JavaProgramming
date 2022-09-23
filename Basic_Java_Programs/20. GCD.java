b3f10d4516836c16996ba967cbab0a50f0cd016c
MOHAMMED THALHA J <96979827+Mohammed-Thalha@users.noreply.github.com>
2022-09-23 06:42:15 +0530

    Create 20. GCD.java

---
+++ Basic_Java_Programs/20. GCD.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+class Main{
+public static void main(String[] args){
+Scanner sc = new Scanner(System.in);
+System.out.println("Enter two numbers : ");
+int num1=sc.nextInt();
+int num2=sc.nextInt();
+int gcd=1;
+for(int i=1;i<=num1 && i<=num2;i++){
+if(num1%i==0 && num2%i==0){
+gcd=i;
+}
+}
+System.out.println("GCD of "+num1+" and "+num2+" is : "+gcd);
+}
+}
