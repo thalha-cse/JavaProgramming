b12361379fa6b907d98a0fb9ae3fa730df96ebfa
MOHAMMED THALHA J <96979827+Mohammed-Thalha@users.noreply.github.com>
2022-11-03 20:31:12 +0530

    Update and rename Basic_Java_Programs/15. Print Alphabet.java to Basic Programs/15. Print Alphabet.java

---
+++ Basic Programs/15. Print Alphabet.java
@@ -0,0 +1,8 @@
+public class Alphabet {
+    public static void main(String[] args){
+        for(char i='A';i<='Z';i++){
+            int a =  i;
+            System.out.print(i+"-"+a+",");
+        }
+    }
+}
--- Basic_Java_Programs/15. Print Alphabet.java
+++
@@ -1,7 +0,0 @@
-class alphabet{
-public static void main(String[] args){
-for(char i='A';i<='Z';i++){
-System.out.println(i+" ");
-}
-}
-}
