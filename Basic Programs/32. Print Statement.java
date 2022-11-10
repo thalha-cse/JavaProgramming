29c3d7851563feaf3a02f59ff83b9fcf5eb27391
ᴍᴏʜᴀᴍᴍᴇᴅ ᴛʜᴀʟʜᴀ <96979827+Mohammed-Thalha@users.noreply.github.com>
2022-11-10 16:11:52 +0530

    Create 32. Print Statement.java

---
+++ Basic Programs/32. Print Statement.java
@@ -0,0 +1,16 @@
+//In every java program statement ends with semicolon but in this program without using semicolon printing the statement
+
+import java.util.Scanner;
+public class PrintStatements {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the first String : ");
+        String str1 = sc.nextLine();
+        System.out.println("Enter the second String : ");
+        String str2 = sc.nextLine();
+        if(System.out.printf(str1)==null){}
+        for(int i=1;i<2;System.out.println(str2)){
+            i++;
+        }
+    }
+}
