b038fb4d48a9dad92f781282a10c3f3e901b77b2
ᴍᴏʜᴀᴍᴍᴇᴅ ᴛʜᴀʟʜᴀ <96979827+Mohammed-Thalha@users.noreply.github.com>
2022-11-16 20:18:51 +0530

    Create 01. Binary to Decimal.java

---
+++ Conversion Programs/01. Binary to Decimal.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+public class Binary_Decimal {
+    Scanner scan;
+    int num;
+    void getVal(){
+        System.out.println("Binary to Decimal");
+        scan = new Scanner(System.in);
+        System.out.println("Enter the number : ");
+        num = Integer.parseInt(scan.nextLine(),2);
+    }
+    void convert(){
+        String decimal = Integer.toString(num);
+        System.out.println("Decimal value is : "+decimal);
+    }
+}
+class MainClas{
+    public static void main(String[] args){
+        Binary_Decimal obj = new Binary_Decimal();
+        obj.getVal();
+        obj.convert();
+    }
+}
