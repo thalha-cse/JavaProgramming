a96babc81a0cb6c57baea07ba5150b13620416eb
ᴍᴏʜᴀᴍᴍᴇᴅ ᴛʜᴀʟʜᴀ <96979827+Mohammed-Thalha@users.noreply.github.com>
2022-11-18 21:38:56 +0530

    Update 02. Binary to Hexadecimal.java

--- Conversion Programs/02. Binary to Hexadecimal.java
+++ Conversion Programs/02. Binary to Hexadecimal.java
@@ -1,22 +1,13 @@
 import java.util.Scanner;
 class Binary_Hexadecimal{
-    Scanner scan;
-	int num;
-	void getVal(){
-		System.out.println("Binary to HexaDecimal");
-		scan = new Scanner(System.in);
-		System.out.println("\nEnter the number :");
-		num = Integer.parseInt(scan.nextLine(), 2);
-	}
-	void convert(){
-		String hexa = Integer.toHexString(num);
-		System.out.println("HexaDecimal Value is : " + hexa);
-	 }
-}
-class Main_Class{
-    public static void main(String... q){
-	Binary_Hexadecimal obj = new Binary_Hexadecimal();
-	obj.getVal();
-	obj.convert();
-}
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int num;
+        System.out.println("Binary to Hexadecimal");
+        System.out.println("Enter the number : ");
+        num = Integer.parseInt(sc.nextLine());
+
+        String hexa = Integer.toHexString(num);
+        System.out.println("Hexadecimal Value is : "+hexa);
+    }
 }
