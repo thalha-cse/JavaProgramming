99ae9a0f4b052fc00c79feefa38a5658df087af5
ᴍᴏʜᴀᴍᴍᴇᴅ ᴛʜᴀʟʜᴀ <96979827+Mohammed-Thalha@users.noreply.github.com>
2022-11-18 20:50:24 +0530

    Update 03. Binary to Octal.java

--- Conversion Programs/03. Binary to Octal.java
+++ Conversion Programs/03. Binary to Octal.java
@@ -1,27 +1,23 @@
 import java.util.Scanner;
- class Binary_Octal
- {
-                 Scanner scan;
+ class Binary_Octal{
+        Scanner scan;
 	int num;
-	void getVal() 
-	{
+	void getVal(){
                      System.out.println("Binary to Octal");
 	    scan = new Scanner(System.in);
  	    System.out.println("\nEnter the number :");
 	    num = Integer.parseInt(scan.nextLine(), 2);
 	}
-	void convert() 
-	{
+	void convert(){
 	        String octal = Integer.toOctalString(num);
 	       System.out.println("Octal Value is : " + octal);
 	}
  }
 class Main_Class
 {
-  public static void main(String... d) 
-    {
+  public static void main(String... d){
         Binary_Octal obj = new Binary_Octal();
         obj.getVal();
         obj.convert();
-   }
+}
 }
